253115edb9176be724457b04cfbc9c17f69974a9
Sunday Brian <brians931@gmail.com>
2019-07-19 16:45:32 +0300

    add tearDown to reset the list to empty after each test

--- test_user.py
+++ test_user.py
@@ -27,7 +27,25 @@ class TestUser(unittest.TestCase):
         test save user to test if the user object is saved into the user list
         '''
         self.new_user.save_user() #saving a user
-        self.assertEqual(len(User.user_list),1)        
+        self.assertEqual(len(User.user_list),1)
+
+    def test_save_multiple_user(self):
+        '''
+        test save multiple to check if we can save multiple user obj to our user_list
+        '''
+
+        self.new_user.save_user()
+        test_user=User("Walter","02_Bishop")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def tearDown(self):
+        '''
+        tearDown method that cleans up after each test has run
+        '''
+        User.user_list=[]    
+
+
 
         
 if __name__=='__main__':
