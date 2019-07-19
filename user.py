87c8191512e0b188668b7fcbf25cb52c7defe4c8
Sunday Brian <brians931@gmail.com>
2019-07-19 15:41:47 +0300

    create the user class to create new instances of a

---
+++ user.py
@@ -0,0 +1,19 @@
+
+
+class User:
+    '''
+    Class that generates a new instance of a passlocker user
+    '''
+
+    '''
+    __init__method that helps us to define properties for our objects
+
+    Args:
+        name:New user name
+        password:New user password
+    '''
+
+    def __init__(self,name,password):
+        
+        self.name=name
+        self.password=password
